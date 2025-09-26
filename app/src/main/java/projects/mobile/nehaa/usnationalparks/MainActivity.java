9d93e3d8720cc360dd9e806572016a5aec4c47f4
nehaau2305 <nehaau2305@gmail.com>
2025-09-26 19:59:41 -0400

    set up the API key in MainActivity to make API calls

--- app/src/main/java/projects/mobile/nehaa/usnationalparks/MainActivity.java
+++ app/src/main/java/projects/mobile/nehaa/usnationalparks/MainActivity.java
@@ -1,6 +1,8 @@
 package projects.mobile.nehaa.usnationalparks;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +13,11 @@ import androidx.core.view.WindowInsetsCompat;
 // set up retrofit for API access and Gson
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
+
+// java io for API call
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 /**
  * MainActivity sets up the main user interface. This will contain the search bar
@@ -23,6 +30,9 @@ public class MainActivity extends AppCompatActivity {
 
     Retrofit retrofit;
     ParkApi parksApi;
+    String apiKey;
+    EditText searchInput;
+    Button searchB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,11 +45,30 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
+        // load unique API key from assets/apikeys.properties
+        try {
+            Properties properties = new Properties();
+            InputStream inputStream = getAssets().open("apikeys.properties");
+            properties.load(inputStream);
+            apiKey = properties.getProperty("NPS_API_KEY", "");
+        } catch (IOException e) {
+            e.printStackTrace();
+            apiKey = "";
+        }
+
+        // retrofit to make API calls
         retrofit = new Retrofit.Builder()
                 .baseUrl("https://developer.nps.gov/api/v1/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
-
         parksApi = retrofit.create(ParkApi.class);
-    }
+
+        // connect UI components using their IDs from activity_main.xml
+        //searchInput = findViewById(R.id.~~);
+        //searchB = findViewById(R.id.~~);
+    } // onCreate
+
+    private void searchForPark(String query) {
+
+    } // searchForPark
 }
