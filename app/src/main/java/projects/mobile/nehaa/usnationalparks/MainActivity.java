f5698444b9af058d174b78a341939c09dec6c616
nehaau2305 <nehaau2305@gmail.com>
2025-09-26 12:20:02 -0400

    set up retrofit in MainActivity to make API calls

--- app/src/main/java/projects/mobile/nehaa/usnationalparks/MainActivity.java
+++ app/src/main/java/projects/mobile/nehaa/usnationalparks/MainActivity.java
@@ -8,7 +8,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+// set up retrofit for API access and Gson
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * MainActivity sets up the main user interface. This will contain the search bar
+ * for users to query their desired national park. Clicking the search button or enter
+ * will navigate them to another activity in which the API response will be displayed.
+ *
+ * @author Nehaa Umapathy
+ */
 public class MainActivity extends AppCompatActivity {
+
+    Retrofit retrofit;
+    ParkApi parksApi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +34,12 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl("https://developer.nps.gov/api/v1/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        parksApi = retrofit.create(ParkApi.class);
     }
 }
